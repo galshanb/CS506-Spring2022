7d800b1b13aa0e9ea53b37a165a9d7dfeb057c3f
galshanb <98421688+galshanb@users.noreply.github.com>
2022-02-01 14:45:02 -0500

    Update library.py

--- 01-python/education/library.py
+++ 01-python/education/library.py
@@ -8,5 +8,13 @@ import tkinter
 import io
 
 def draw_library():
+    
     print("library not found")
     return
+    img= Image.open("lib.jpg")
+    img.thumbnail((1200,800))
+    
+    bi = io.BytesIO()
+    img.save(bi, format = "PNG")
+    
+    #layout = [[ sg.Text ("Library", font = ("Ariel",20)),[sg.Image(data
