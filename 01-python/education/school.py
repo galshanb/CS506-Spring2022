e3b4e315d50a40cf4797b622df13deab6b4f94a7
JingyuZ <90217670+Jinnyzg@users.noreply.github.com>
2022-02-01 14:36:16 -0500

    Update school.py
    import library

--- 01-python/education/school.py
+++ 01-python/education/school.py
@@ -1,3 +1,12 @@
+import math
+import cairo
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
+import PysimpleGUI as sg
+import os
+import tkinter
+import io
+
 def draw_school():
     print("school not found")
     return
